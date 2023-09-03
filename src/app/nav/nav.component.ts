c86043b810ae546702f2deb1992a44e02b9ccd72
Isha <ishagoyal510@gmail.com>
2023-09-03 13:45:35 +0530

    Selectors as attribute and class

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
+  // selector: 'app-nav',
+  // selector: '[app-nav]',
+  selector: '.app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
