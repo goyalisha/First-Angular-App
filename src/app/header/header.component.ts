80f9a600eba75951205eba2031fd65ca323f20a3
Isha <ishagoyal510@gmail.com>
2023-09-03 14:12:23 +0530

    Added property binding in header and footer components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  heading: string = 'First step towards Mastery of Angular!';
+  source: string = '../../assets/angular.jpg';
 
+  getHeading() {
+    return 'First step towards Mastery of Angular!';
+  }
 }
